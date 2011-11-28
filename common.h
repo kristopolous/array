beb9e2aedf8d17ecd1c0fc8180ac07b3e26e8c5e
Chris McKenzie <kristopolous@yahoo.com>
2011-11-28 14:27:15 -0800

    Adding common.h

---
+++ common.h
@@ -0,0 +1,18 @@
+/* Array
+ *	array management for shell scripts
+ *
+ * (c) Copyright 2005, 2008, 2011 Christopher J. McKenzie under the terms of the
+ *  GNU Public License, incorporated herein by reference
+ */
+#ifndef _COMMON_H_1231231313131415151515
+#define _COMMON_H_1231231313131415151515
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+int doerror();
+#endif
